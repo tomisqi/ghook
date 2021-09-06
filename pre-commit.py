f55cc60453848dc621c706dd3eaf4bbdd1d9fe9d
Tony Fiallos <afiallosh@gmail.com>
2021-09-06 11:29:24 -0500

    First commit

---
+++ pre-commit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""  
+     Pre-Commit git hook
+"""
+__version__ = '0.1'
+
+import sys
+
+
+def main():
+    print("Hello world!")    
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
+
