c0a81474845b68764c9e05d8c193fbea0fa381b2
Tony Fiallos <afiallosh@gmail.com>
2021-09-08 23:56:22 -0500

    Fail or Pass string

--- pre-commit.py
+++ pre-commit.py
@@ -14,23 +14,32 @@ import subprocess
 
 NO_COMMIT_STR = "@nocommit"
 
-def main():
-    print("Pre-Commit hook check...")
+def check(gitdiff, nocommitStr):
+    success = True
+    
+    findex = gitdiff.find(nocommitStr)
+    if (findex >= 0):
+        # We found something, print the line and return error.
+        (startIdx, endIdx) = (gitdiff.rfind('\n', 0, findex) + 1, gitdiff.find('\n', findex, -1))
+        print(nocommitStr + " found: " + gitdiff[startIdx:endIdx])
+        success = False
 
+    return success
+
+def main():
     cmd = "git diff --staged"    
     cmdres = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     stdout = cmdres.stdout
     gitdiff = stdout.decode('UTF-8') # stdout is returned as bytes but we want a string.
-    
-    findex = gitdiff.find(NO_COMMIT_STR)
-    if (findex >= 0):
-        # We found something, print the line and exit with error.
-        (startIdx, endIdx) = (gitdiff.rfind('\n', 0, findex) + 1, gitdiff.find('\n', findex, -1))
-        print(NO_COMMIT_STR + " found: " + gitdiff[startIdx:endIdx])
+
+    success = check(gitdiff, NO_COMMIT_STR)
+
+    resultStr = "Pass" if success else "Fail"
+    print("Pre-Commit hook check..." + resultStr + ".")
+    if (not success):
         sys.exit(1)
-    
+    sys.exit(0)
     
 if __name__ == '__main__':
     main()
-    sys.exit(0)
 
